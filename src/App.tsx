dabb49eec9cb89acba8b79f1ce976e79ecebd179
Seremet Alexandru <andutzu.268@gmail.com>
2022-08-16 21:12:05 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
 import './App.css'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Navigate } from 'react-router'
 
-function App() {
-  return <div></div>
+import LandingPage from './components/LandingPage/LandingPage'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/landing-page' element={<LandingPage />} />
+        <Route path='/' element={<Navigate to='/landing-page' />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+	return <div>qwdqwdwqdwqd</div>
+}
+
+export default LandingPage
